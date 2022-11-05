04b8f09ec3e7d1512619da75afdd36687510a75c
Pranjal <happybarnwal007@gmail.com>
2022-11-05 21:31:47 +0530

    nav-item hover changes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Navbar from './components/navbar/Navbar';
 import './App.css';
-
+import About from './components/about/About';
 function App() {
   return (
     <div className="main">
-      
+      {/* try z-index on nav and abput using div */}
      <Navbar/>
+     {/* <About/> */}
     </div>
   );
 }
---
+++ src/components/about/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import "../about/About.css"
+const about = () => {
+  return (
+    <div>
+      <section className="order1">
+        <div className="order1-wrapper">
+        <h1 className='heading'>About Us</h1>
+         <div className="sub-heading">
+          <b>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, aut.</b>
+          <div>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque, saepe repellendus, placeat quam ipsam enim obcaecati, nesciunt corporis cumque necessitatibus pariatur? Fugiat eum quibusdam minus. Doloribus atque praesentium eligendi corporis, nemo molestias debitis maiores, blanditiis officiis quia architecto. Deleniti voluptates blanditiis hic natus quas explicabo beatae inventore? Excepturi ea id perferendis, explicabo sit unde eum dolorem vitae corporis suscipit facilis sequi mollitia, ipsam quos ipsum cupiditate tenetur rerum, quod atque voluptate ducimus! Ipsum odit repellendus nisi libero ducimus at debitis corrupti perspiciatis modi reprehenderit qui excepturi voluptas, quidem, omnis dolore.
+          </div>
+          </div>
+        </div>
+      </section>
+      <section className="order2"></section>
+    </div>
+  )
+}
+
+export default about
