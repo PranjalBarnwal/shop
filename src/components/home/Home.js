e5606c418a9b956e23fe90102a1a62a25f459ec7
Pranjal <happybarnwal007@gmail.com>
2022-11-15 01:59:40 +0530

    home page

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,10 +4,14 @@ import bathroom from "../img/bathroom.webp"
 import "./Home.css"
 const Home = () => {
   return (
-    <section className='main'>\
-    <p>Experience the best quality and price at Sourav Traders</p>
-        <div className="left"></div>
-       <div className="right"><img src={bathroom} alt="" style={{width:"700px"}}/></div>
+    <section className='main'>
+   <div className="left">
+    <p>Experience the best <strong>Quality</strong> and <strong>Price</strong> at <span>Sourav Traders</span></p>
+    </div>
+   <div className="right">
+    <img className='home-img' src={landingimg} alt="" />
+   </div>
+   
     </section>
  )
 }
