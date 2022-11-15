fceb4ce887f9be157ac24833d87db6417216a8e7
Pranjal <happybarnwal007@gmail.com>
2022-11-15 13:58:10 +0530

    home page redesigned

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,15 +4,16 @@ import bathroom from "../img/bathroom.webp"
 import "./Home.css"
 const Home = () => {
   return (
-    <section className='main'>
-   <div className="left">
-    <p>Experience the best <strong>Quality</strong> and <strong>Price</strong> at <span>Sourav Traders</span></p>
-    </div>
-   <div className="right">
+ <section className="main">
+  <div className="left">
+    <div className="left-top">Experience the best <strong>Quality</strong> and <strong>Price</strong> at</div>
+    <div className="left-bottom">Sourav Traders</div>
+  </div>
+
+  <div className="right">
     <img className='home-img' src={landingimg} alt="" />
-   </div>
-   
-    </section>
+  </div>
+ </section>
  )
 }
 
