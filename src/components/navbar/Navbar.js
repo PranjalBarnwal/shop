dc534604932e7a7ed368a7c11c2c34a2cc2de103
Pranjal <happybarnwal007@gmail.com>
2022-11-16 20:59:34 +0530

    nav responsive and home responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,51 +3,59 @@ import logo1 from "../img/logo1.png";
 import "./Navbar.css";
 function Navbar() {
   return (
-    <div>
-      <nav className="navbar">
-        <ul className="nav-list">
-          {/* <li className="name"> */}
-          <div className="name">
-            <img src={logo1} alt="" className="logo" />
-          </div>
-          {/* </li> */}
-          {/* <img src={logo} alt="" className="logo" /> */}
-          <li>
-            <a className="nav-items" href="#">
-              Home
-            </a>
-          </li>
-          <li>
-            <a className="nav-items our-products" href="#Products">Our Products</a>
-            <div className="wrapper">
-            <div className="products-section">
-           <div className="sides"></div>
-           <div className="img "></div>
-           <div className="img "></div>
-           <div className="img"></div>
-           <div className="img"></div>
-           <div className="img"></div>
-           <div className="sides"></div>
-              </div>  
-            </div>
-          </li>
+    <nav className="navbar">
+      <ul className="nav-list">
+        {/* <li className="name"> */}
+        <div className="name">
+          <img src={logo1} alt="" className="logo" />
+        </div>
+       
+       
+       
+        <a href="#" className="toggle-button">
+          <span className="bar"></span>
+          <span className="bar"></span>
+          <span className="bar"></span>
+        </a>
+  
+       
+       
         <li>
-            <a className="nav-items" href="#AboutUs">
-              About Us
-            </a>
-          </li>
-          <li>
-            <a className="nav-items" href="#Contact">
-              Contact Us
-            </a>
-          </li>
-          <li>
-            <button className="locate">Locate Our Stores</button>
-          </li>
-        </ul>
-      </nav>
-      {/* <hr /> */}
-    </div>
+          <a className="nav-items" href="#">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="nav-items our-products" href="#Products">
+            Our Products
+          </a>
+          <div className="wrapper">
+            <div className="products-section">
+              <div className="sides"></div>
+              <div className="img "></div>
+              <div className="img "></div>
+              <div className="img"></div>
+              <div className="img"></div>
+              <div className="img"></div>
+              <div className="sides"></div>
+            </div>
+          </div>
+        </li>
+        <li>
+          <a className="nav-items" href="#AboutUs">
+            About Us
+          </a>
+        </li>
+        <li>
+          <a className="nav-items" href="#Contact">
+            Contact Us
+          </a>
+        </li>
+        <li>
+          <button className="locate">Locate Our Stores</button>
+        </li>
+      </ul>
+    </nav>
   );
 }
 
