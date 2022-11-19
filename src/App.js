2489f7c2be0417a090ae1a5ee6d2f6718aa590d8
Pranjal <happybarnwal007@gmail.com>
2022-11-19 15:25:41 +0530

    home page

--- src/App.js
+++ src/App.js
@@ -4,16 +4,22 @@ import ContactUs from './components/contact/ContactUs';
 import About from './components/about/About';
 import Home from "./components/home/Home"
 import Products from './components/products/Products';
+import {Routes,Route,BrowserRouter as Router} from "react-router-dom";
 function App() {
   return (
+    <Router>
     <div className="main">
-      {/* try z-index on nav and abput using div */}
+    
      <Navbar/>
-     {/* <Home/> */}
-     {/* <About/> */}
-     {/* <ContactUs/> */}
-     <Products/>
+     <Routes>
+       <Route path="/" element={<Home/>}></Route>
+       <Route path="/products" element={<Products/>}></Route>
+       <Route path="/about" element={<About/>}></Route>
+       <Route path="/contact" element={<ContactUs/>}></Route>
+       <Route path="/locate" element={<ContactUs/>}></Route>
+    </Routes>
     </div>
+    </Router>
   );
 }
 
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,40 +1,36 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import logo1 from "../img/logo1.png";
 import "./Navbar.css";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  // // const toggleButton=document.querySelector(".toggle-button");
-  // // const navlist = document.getElementsByClassName('.nav-list')[0];
-  // const navlist = useRef();
-  const [toggle,setToggle]=useState(false);
-  const toggleFunc=()=>{
-  setToggle(!toggle);
- }
-  // console.log(navlist);
-  // // toggleButton.addEventListener('click',()=>{
-  // // })
+  
+  const [toggle, setToggle] = useState(false);
+  const toggleFunc = () => {
+    setToggle(!toggle);
+  };
+ 
 
   return (
     <nav className="navbar">
       <div className="name">
         <img src={logo1} alt="" className="logo" />
       </div>
-        <a href="#" className="toggle-button" onClick={toggleFunc}>
-          <span className="bar"></span>
-          <span className="bar"></span>
-          <span className="bar"></span>
-        </a>
-      <ul className={`nav-list ${toggle?'active':''}`} >
-
+      <a href="#" className="toggle-button" onClick={toggleFunc}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </a>
+      <ul className={`nav-list ${toggle ? "active" : ""}`}>
         <li>
-          <a className="nav-items" href="#">
+          <Link onClick={toggleFunc}  to="/" className="nav-items">
             Home
-          </a>
+          </Link>
         </li>
         <li>
-          <a className="nav-items our-products" href="#Products">
+          <Link onClick={toggleFunc}  to="/products" className="nav-items our-products">
             Our Products
-          </a>
+          </Link>
           <div className="wrapper">
             <div className="products-section">
               <div className="sides"></div>
@@ -48,17 +44,21 @@ function Navbar() {
           </div>
         </li>
         <li>
-          <a className="nav-items" href="#AboutUs">
+          <Link onClick={toggleFunc} to="/about" className="nav-items">
             About Us
-          </a>
+          </Link>
         </li>
         <li>
-          <a className="nav-items" href="#Contact">
+          <Link onClick={toggleFunc}  to="/contact" className="nav-items">
             Contact Us
-          </a>
+          </Link>
         </li>
         <li>
-          <button className="locate">Locate Our Stores</button>
+          
+            <Link onClick={toggleFunc}  to="/contact" className="locate" style={{}}>
+              Locate Our Store
+            </Link>
+          
         </li>
       </ul>
     </nav>
